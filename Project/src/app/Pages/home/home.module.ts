e29e76fca637a6bc1cbd5c29346bd9a8dece7249
UClaudio <107913451+UClaudio@users.noreply.github.com>
2022-09-19 17:05:45 +0200

    posts features

--- Project/src/app/Pages/home/home.module.ts
+++ Project/src/app/Pages/home/home.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { NewPostComponent } from './new-post/new-post.component';
+import { PostComponent } from './post/post.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    NewPostComponent
+    NewPostComponent,
+    PostComponent
   ],
   imports: [
     CommonModule,
