57d2a91d8799f12fe0e4a55e8f7dac3f2b2cbf31
77DDS77 <107913330+77DDS77@users.noreply.github.com>
2022-09-20 11:56:51 +0200

    async validators register form

--- Project/src/app/Pages/Auth/register/register.component.ts
+++ Project/src/app/Pages/Auth/register/register.component.ts
@@ -1,59 +1,104 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/Classes/user';
 import { AuthService } from 'src/app/Services/auth.service';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
-  form!:FormGroup
-  isFormValid!:boolean;
+  form!: FormGroup;
+  isFormValid!: boolean;
+  allUsers: User[] = [];
 
   constructor(
     private authSrv: AuthService,
+    private userSrv: UserService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.form = new FormGroup(
-      {
-        name: new FormControl(null, Validators.required),
-        lastname: new FormControl(null, Validators.required),
-        username: new FormControl(null, Validators.required),
-        avatar: new FormControl(null, Validators.required),
-        email: new FormControl(null, Validators.required),
-        password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
-      }
-    )
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      lastname: new FormControl(null, Validators.required),
+      username: new FormControl(null, [Validators.required], this.usernameValidator),
+      avatar: new FormControl(null, Validators.required),
+      email: new FormControl(null,[ Validators.required], this.emailValidator),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+    });
+    console.log(this.form);
   }
 
-  submit(){
-    console.log(this.form.valid);
+  checkValidity(input:string):boolean{
+    return (this.form.get(input)?.invalid
+      && this.form.get(input)?.touched
+      && this.form.get(input)?.dirty) || false
+  }
 
-    if(this.form.valid){
-      this.authSrv.register(new User(
-        this.form.value.name,
-        this.form.value.lastname,
-        this.form.value.username,
-        this.form.value.avatar,
-        this.form.value.email,
-        this.form.value.password
-      ))
-      .subscribe(res => {
-        console.log(res);
-        this.authSrv.saveAccessData(res)
-        this.router.navigate(['/'])
+  //async username validator
+  usernameValidator = (control:AbstractControl) => {
+    return new Promise<ValidationErrors | null>((resolve) => {
+      this.userSrv.getAllUsers()
+      .subscribe(users => {
+        for (let user of users) {
+          if (user.username == control.value) {
+            resolve ({usernameIsTaken:true})
+          }
+        }
+        resolve(null)
       })
-    }else{
+    })
+  }
+
+  //async email validator
+  emailValidator = (control:AbstractControl) => {
+    return new Promise<ValidationErrors | null>((resolve) => {
+      this.userSrv.getAllUsers()
+      .subscribe(users => {
+        for (let user of users) {
+          if (user.email == control.value) {
+            resolve ({emailIsTaken:true})
+          }
+        }
+        resolve(null)
+      })
+    })
+  }
+
+  submit() {
+    if (this.form.valid) {
+      this.authSrv
+        .register(
+          new User(
+            this.form.value.name,
+            this.form.value.lastname,
+            this.form.value.username,
+            this.form.value.avatar,
+            this.form.value.email,
+            this.form.value.password
+          )
+        )
+        .subscribe((res) => {
+          console.log(res);
+          this.authSrv.saveAccessData(res);
+          this.router.navigate(['/']);
+        });
+    } else {
       console.log('errore form non valido');
       this.isFormValid = false;
-
     }
   }
-
 }
